e989ef1e1bff7afb3bee663bcc1232f28a825bdb
James Dalboth <James.Dalboth@gmail.com>
2019-05-28 16:03:02 +0100

    [PR] CI/CD migrated to CircleCI for consistency (#4)
    * Add circleci folder
    
    * Fix typo in config.yml
    
    * Change config.yml to use gradle
    
    * Update TestController/Resource to be compliant with checkstyle
    
    * Updated README.md
    
    * Updated README.md
    
    * Modify Dockerfile to include checkstyle.xml
    
    * update circleci yaml for deployment
    
    * Fix format of yaml

--- src/main/java/backend/controllers/TestController.java
+++ src/main/java/backend/controllers/TestController.java
@@ -1,17 +1,17 @@
 package backend.controllers;
 
-import backend.resources.testResource;
+import backend.resources.TestResource;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class testController {
+public class TestController {
 
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/test")
-  public testResource testResource() {
-    return new testResource(counter.incrementAndGet());
+  public TestResource testResource() {
+    return new TestResource(counter.incrementAndGet());
   }
 }
