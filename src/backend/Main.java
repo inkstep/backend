dae700ae8cb9b1e870aa616beb60b9b8593094ee
James Dalboth <James.Dalboth@gmail.com>
2019-05-31 12:00:10 +0100

    [PR] Implement versioning (#19)
    * Move everything into the v1 path
    
    * Fix tests
    
    * Fix package name style issues

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -1,15 +1,12 @@
 import static spark.Spark.get;
+import static spark.Spark.path;
 import static spark.Spark.put;
+import static version.v1.Journey.getJourneyRoute;
+import static version.v1.Journey.putJourneyRoute;
+import static version.v1.Users.putUserRoute;
 
 import database.PostgreDatabaseConnection;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import version.v1.Users;
 
 
 public class Main {
@@ -19,93 +16,12 @@ public class Main {
 
     Users.setDatabaseConnection(new PostgreDatabaseConnection());
 
-    get("/test", new Route() {
-      public Object handle(Request request, Response response)
-          throws Exception {
-        return "User: username=test, email=test@test.net";
-      }
-    });
+    path("/version/v1", () -> {
+      put("/user", putUserRoute());
 
-    put("/user", new Route() {
-      public Object handle(Request request, Response response)
-          throws Exception {
-        System.out.println("Request received PUT user");
-        String userName = request.queryParams("name");
-        if (userName == null) {
-          return "name query not supplied";
-        }
-
-        String passphrase = "JimmyHarryDannyMatty";
-
-        JSONObject userResponse = new JSONObject();
-
-        userResponse.put("name", userName);
-        userResponse.put("passphrase", passphrase);
-
-        return userResponse.toJSONString();
-      }
-    });
-
-    get("/journey", new Route() {
-      @Override
-      public Object handle(Request request, Response response)
-          throws Exception {
-        System.out.println("Request received GET journey");
-
-        JSONObject journeyObject = new JSONObject();
-
-        journeyObject.put("user_name", "Jimmy");
-        journeyObject.put("artist_name", "Rickay");
-        journeyObject.put("artist_email", "Rickay@theemail.com");
-        journeyObject.put("tattoo", "star");
-        journeyObject.put("size", "10cm");
-        journeyObject.put("pos", "neck");
-        journeyObject.put("desc", "I like tattoo's");
-
-        return journeyObject.toJSONString();
-      }
-    });
-
-    put("/journey", new Route() {
-      public Object handle(Request request, Response response)
-          throws Exception {
-        System.out.println("Request received PUT journey");
-
-        String body = request.body();
-
-        System.out.println(body);
-
-        JSONParser parser = new JSONParser();
-        JSONObject requestjson = (JSONObject) parser.parse(body);
-
-        System.out.println(requestjson.toJSONString());
-
-        String userName = (String) requestjson.get("user_name");
-        String artistName = (String) requestjson.get("artist_name");
-        String artistEmail = (String) requestjson.get("artist_email");
-        String tattoo = (String) requestjson.get("tattoo");
-        String size = (String) requestjson.get("size");
-        String position = (String) requestjson.get("pos");
-        String description = (String) requestjson.get("desc");
-
-        requestjson.get("images");
-
-        List<String> filenames = new ArrayList<>();
-        //filenames.add("src/resources/image1.jpg");
-
-        Journey.newJourney(
-            userName,
-            artistName,
-            artistEmail,
-            tattoo,
-            size,
-            position,
-            description,
-            filenames
-        );
-
-        return "{}";
-      }
+      get("/journey", getJourneyRoute());
+      put("/journey", putJourneyRoute());
     });
   }
+
 }
