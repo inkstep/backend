912f8d982fc74f033955d4b098a38efbdaa0044c
Matthew Malarkey <mm5917@imperial.ac.uk>
2019-06-03 15:47:37 +0100

    Add data model for artists and studios

---
+++ src/backend/model/Artist.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Artist {
+  final String name;
+  final Studio studio;
+
+  public Artist(String name, Studio studio) {
+    this.name = name;
+    this.studio = studio;
+  }
+}
---
+++ src/backend/model/Studio.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Studio {
+  final String name;
+
+  public Studio(String name) {
+    this.name = name;
+  }
+}
