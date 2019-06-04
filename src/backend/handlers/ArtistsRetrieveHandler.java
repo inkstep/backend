be042dcb9b46771ac16bf615e2dd5551bdfd1ec4
Daniel Hails <daniel@hails.info>
2019-06-04 22:33:05 +0100

    Add retrieval of Artists via Handler

---
+++ src/backend/handlers/ArtistsRetrieveHandler.java
@@ -0,0 +1,34 @@
+package handlers;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Artist;
+import store.InkstepStore;
+
+public class ArtistsRetrieveHandler extends AbstractRequestHandler<EmptyPayload> {
+
+  public ArtistsRetrieveHandler(InkstepStore store) {
+    super(EmptyPayload.class, store);
+  }
+
+  @Override
+  protected Answer processImpl(EmptyPayload empty, Map<String, String> urlParams) {
+    List<Artist> artists = store.getArtists();
+
+    final String jsonString;
+    try {
+        jsonString = new ObjectMapper().writeValueAsString(artists);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Answer.empty(BAD_REQUEST);
+    }
+
+    return Answer.ok(jsonString);
+  }
+}
