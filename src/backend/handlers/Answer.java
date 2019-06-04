6e3de9588f46e1a7a52c339497d458b5e4755246
Daniel Hails <daniel@hails.info>
2019-06-04 20:52:07 +0100

    Add equals, hashcode and override to Answer

--- src/backend/handlers/Answer.java
+++ src/backend/handlers/Answer.java
@@ -1,7 +1,8 @@
 package handlers;
 
-public class Answer {
+import java.util.Objects;
 
+public class Answer {
   private int code;
   private String body;
 
@@ -17,12 +18,37 @@ public class Answer {
   public static Answer ok(String body) {
     return new Answer(200, body);
   }
-
+  
   public String getBody() {
-    return body;
+    return this.body;
   }
 
   public int getCode() {
-    return code;
+    return this.code;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof Answer)) {
+      return false;
+    }
+    Answer answer = (Answer) o;
+    return code == answer.code && Objects.equals(body, answer.body);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(code, body);
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+      " code='" + getCode() + "'" +
+      ", body='" + getBody() + "'" +
+      "}";
   }
 }
