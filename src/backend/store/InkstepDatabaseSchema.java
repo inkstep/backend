5cd1d098cd60bfbe6d5bdb4d3ef94f1191725611
Matthew Malarkey <mm5917@imperial.ac.uk>
2019-06-05 17:21:34 +0100

    Remove unused all column arrays from schema

--- src/backend/store/InkstepDatabaseSchema.java
+++ src/backend/store/InkstepDatabaseSchema.java
@@ -17,8 +17,6 @@ public class InkstepDatabaseSchema {
   public static final DbColumn USER_EMAIL = USERS.addColumn("Email", "varchar", 45);
   public static final DbColumn USER_PHONE = USERS.addColumn("Phone", "varchar", 45);
   public static final DbColumn USER_PASSPHRASE = USERS.addColumn("Passphrase", "varchar", 45);
-  public static final DbColumn[] USER_COLUMNS =
-    new DbColumn[] {USER_ID, USER_NAME, USER_EMAIL, USER_PHONE, USER_PASSPHRASE};
 
   // Artists
   public static final DbTable ARTISTS = SCHEMA.addTable("artists");
@@ -26,8 +24,6 @@ public class InkstepDatabaseSchema {
   public static final DbColumn ARTIST_STUDIO_ID = ARTISTS.addColumn("StudioID", "int", 11);
   public static final DbColumn ARTIST_NAME = ARTISTS.addColumn("Name", "varchar", 127);
   public static final DbColumn ARTIST_EMAIL = ARTISTS.addColumn("Email", "varchar", 45);
-  public static final DbColumn[] ARTIST_COLUMNS =
-    new DbColumn[] {ARTIST_ID, ARTIST_STUDIO_ID, ARTIST_NAME, ARTIST_EMAIL};
 
   // Studios
   public static final DbTable STUDIOS = SCHEMA.addTable("studios");
@@ -46,9 +42,6 @@ public class InkstepDatabaseSchema {
   public static final DbColumn JNY_AVAIL = JOURNEYS.addColumn("Availability", "varchar", 45);
   public static final DbColumn JNY_DEPOSIT = JOURNEYS.addColumn("Deposit", "varchar", 45);
   public static final DbColumn JNY_NO_REF_IMAGES = JOURNEYS.addColumn("NoRefImgs", "int", 11);
-  public static final DbColumn[] JOURNEY_COLUMNS =
-    new DbColumn[] {JNY_ID, JNY_USER_ID, JNY_ARTIST_ID, JNY_DESCRIPTION, JNY_SIZE, JNY_POSITION,
-      JNY_AVAIL, JNY_DEPOSIT, JNY_NO_REF_IMAGES};
 
   // Journey Images
   public static final DbTable JOURNEY_IMAGES = SCHEMA.addTable("journey_images");
@@ -56,6 +49,4 @@ public class InkstepDatabaseSchema {
   public static final DbColumn JNY_IMAGE_JNY_ID = JOURNEY_IMAGES.addColumn("JourneyID", "int", 11);
   public static final DbColumn JNY_IMAGE_DATA =
     JOURNEY_IMAGES.addColumn("ImageData", "mediumtext", 65536);
-  public static final DbColumn[] JOURNEY_IMAGE_COLUMNS =
-    new DbColumn[] {JNY_IMAGE_ID, JNY_IMAGE_JNY_ID};
 }
