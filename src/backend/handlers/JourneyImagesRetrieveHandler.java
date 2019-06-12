4b8ed6d5c330dd368cfe50b8271f336ffd9293c6
James Dalboth <James.Dalboth@gmail.com>
2019-06-12 16:19:30 +0100

    Send smaller sized image

--- src/backend/handlers/JourneyImagesRetrieveHandler.java
+++ src/backend/handlers/JourneyImagesRetrieveHandler.java
@@ -1,8 +1,19 @@
 package handlers;
 
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
-import model.Journey;
+import javax.imageio.ImageIO;
 import store.InkstepStore;
 
 public class JourneyImagesRetrieveHandler extends AbstractRequestHandler<EmptyPayload> {
@@ -11,12 +22,46 @@ public class JourneyImagesRetrieveHandler extends AbstractRequestHandler<EmptyPa
     super(EmptyPayload.class, store);
   }
 
-  synchronized
   @Override protected Answer processImpl(EmptyPayload userToBe, Map<String, String> urlParams) {
     System.out.println("Attempting to retrieve images for journey_id " + urlParams.get(":id"));
 
     List<String> imageData = store.getImagesFromJourneyId(Integer.valueOf(urlParams.get(":id")));
 
-    return Answer.ok(dataToJson(imageData));
+    List<BufferedImage> images = new ArrayList<>();
+
+    for (String data : imageData) {
+      byte[] decodedBytes = Base64.getDecoder().decode(data);
+
+      ByteArrayInputStream bis = new ByteArrayInputStream(decodedBytes);
+      try {
+        BufferedImage image = ImageIO.read(bis);
+        images.add(image);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    List<String> resizedData = new ArrayList<>();
+
+    for (BufferedImage image : images) {
+      BufferedImage after = new BufferedImage(
+        image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+      AffineTransform at = new AffineTransform();
+      at.scale(2.0, 2.0);
+      AffineTransformOp scaleOp =
+        new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+      after = scaleOp.filter(image, after);
+
+      WritableRaster wr = after.getRaster();
+      DataBufferByte data   = (DataBufferByte) wr.getDataBuffer();
+
+      try {
+        resizedData.add(new String(Base64.getEncoder().encode(data.getData()), "UTF-8"));
+      } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return Answer.ok(dataToJson(resizedData));
   }
 }
