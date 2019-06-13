70abd51e17ca44a339ff152f74932c07ed6e5417
James Dalboth <James.Dalboth@gmail.com>
2019-06-13 16:00:09 +0100

    Transition automaticall on journeys

--- src/backend/handlers/JourneysRetrieveHandler.java
+++ src/backend/handlers/JourneysRetrieveHandler.java
@@ -1,6 +1,9 @@
 package handlers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import model.Journey;
+import model.JourneyStage;
 import store.InkstepStore;
 
 import java.util.List;
@@ -16,6 +19,30 @@ public class JourneysRetrieveHandler extends AbstractRequestHandler<EmptyPayload
     System.out.println("Retrieving journeys");
 
     List<Journey> journeys = store.getJourneysForUserID(Integer.valueOf(urlParams.get("user")));
+
+    for (Journey journey : journeys) {
+      if (journey.stage == JourneyStage.AppointmentBooked) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+        LocalDateTime date = LocalDateTime.parse(journey.bookingDate, dateFormatter);
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        if (localDateTime.isAfter(date)) {
+          journey.stage = JourneyStage.Aftercare;
+          store.updateStage(journey.journeyID, JourneyStage.Aftercare);
+        }
+      } else if (journey.stage == JourneyStage.Aftercare) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+        LocalDateTime date = LocalDateTime.parse(journey.bookingDate, dateFormatter);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime doneDate = date.plusDays(31);
+
+        if (localDateTime.isAfter(doneDate)) {
+          journey.stage = JourneyStage.Healed;
+          store.updateStage(journey.journeyID, JourneyStage.Healed);
+        }
+      }
+    }
+
     return Answer.ok(dataToJson(journeys));
   }
 }
