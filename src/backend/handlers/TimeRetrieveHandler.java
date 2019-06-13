9d495faaa2b6d861872fff5d2d50cdd1211a10ef
James Dalboth <James.Dalboth@gmail.com>
2019-06-13 18:48:29 +0100

    Set timezone to london timezone

--- src/backend/handlers/TimeRetrieveHandler.java
+++ src/backend/handlers/TimeRetrieveHandler.java
@@ -1,6 +1,7 @@
 package handlers;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +17,7 @@ public class TimeRetrieveHandler extends AbstractRequestHandler<EmptyPayload> {
   protected Answer processImpl(EmptyPayload value, Map<String, String> queryParams) {
     Map<String, String> responseMap = new HashMap<String, String>() {{
       DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
-      put("image_id", LocalDateTime.now().format(dateFormatter));
+      put("image_id", LocalDateTime.now(ZoneId.of("Europe/London")).format(dateFormatter));
     }};
 
     return Answer.ok(dataToJson(responseMap));
