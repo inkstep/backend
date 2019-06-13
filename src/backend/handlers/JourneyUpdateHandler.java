cd1faf6179f40d079229d4931847f7a303be1d9a
Daniel Hails <daniel@hails.info>
2019-06-13 18:19:42 +0100

    Add firebase sdk

--- src/backend/handlers/JourneyUpdateHandler.java
+++ src/backend/handlers/JourneyUpdateHandler.java
@@ -3,8 +3,13 @@ package handlers;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
 import email.JourneyMail;
+import model.Journey;
 import model.JourneyStage;
+import model.User;
 import model.Validatable;
 import store.InkstepStore;
 
@@ -21,7 +26,29 @@ public class JourneyUpdateHandler
   protected Answer processImpl(Payload request, Map<String, String> urlParams) {
     int journeyId = Integer.valueOf(urlParams.get(":id"));
     JourneyStage newStage = JourneyStage.values()[request.getStage()];
+
     store.updateStage(journeyId, newStage);
+
+    Journey j = store.getJourneyFromId(journeyId);
+    User u = store.getUserFromID(j.userID);
+
+    // See documentation on defining a message payload.
+    Message message = Message.builder()
+      .putData("score", "850")
+      .putData("time", "2:45")
+      .setToken(u.token)
+      .build();
+
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    String response = null;
+    try {
+      response = FirebaseMessaging.getInstance().send(message);
+    } catch (FirebaseMessagingException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Sent notification due to update stage: " + response);
+
     Map<String, String> responseMap = new HashMap<String, String>() {{
       put("JourneyID", String.valueOf(journeyId));
     }};
