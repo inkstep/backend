22231a9f9b13338e2f5ec516c0f42d221481abee
Daniel Hails <daniel@hails.info>
2019-06-18 21:27:39 +0100

    More Answer builders and extraction of magic numbers

--- src/backend/handlers/Answer.java
+++ src/backend/handlers/Answer.java
@@ -8,16 +8,23 @@ import java.util.Objects;
 
 public abstract class Answer {
 
+  public static final int BAD_USER = 400;
+  public static final int SUCCESS = 200;
+
   public static Answer code(int code) {
     return new StringAnswer(code, "");
   }
 
   public static Answer ok(String body) {
-    return new StringAnswer(200, body);
+    return new StringAnswer(SUCCESS, body);
   }
 
   public static Answer png(BufferedImage img) {
     return new ImageAnswer("image/png", img);
+  }
+
+  public static Answer userError(String explanation) {
+    return new StringAnswer(BAD_USER, explanation);
   }
 
   public abstract void update(Response response);
