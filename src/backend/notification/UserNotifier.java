ff595b70b2010922432367388819a40b441ae059
Matthew Malarkey <mm5917@imperial.ac.uk>
2019-06-19 20:58:34 +0100

    Improve style of builder methods

--- src/backend/notification/UserNotifier.java
+++ src/backend/notification/UserNotifier.java
@@ -14,7 +14,6 @@ public class UserNotifier {
   }
 
   public boolean notifyStage(Artist artist, Journey journey, JourneyStage stage) {
-
     String title = artist.name + " has sent an update!";
     String body = "A";
     if (stage == JourneyStage.QuoteReceived) {
@@ -24,29 +23,30 @@ public class UserNotifier {
     }
     body += "has been sent for your " + journey.tattooDesc + " tattoo.";
 
-    ApnsConfig appleConfig = ApnsConfig.builder().setAps(
-            Aps.builder().setBadge(1).setAlert(
-                    ApsAlert.builder().setTitle(title)
-                      .setBody(body)
-                      .build()
-            ).setSound("note").build())
-    .putCustomData("journey", String.valueOf(journey.journeyID))
-    .build();
+    ApsAlert alert = ApsAlert.builder()
+      .setTitle(title)
+      .setBody(body)
+      .build();
+
+    Aps aps = Aps.builder()
+      .setBadge(1)
+      .setAlert(alert)
+      .setSound("note").build();
+
+    ApnsConfig appleConfig = ApnsConfig.builder()
+      .setAps(aps)
+      .putCustomData("journey", String.valueOf(journey.journeyID))
+      .build();
 
     Message message = Message.builder()
       .putData("journey", String.valueOf(journey.journeyID))
       .putData("click_action", "FLUTTER_NOTIFICATION_CLICK")
-      .setNotification(
-        new Notification(
-          title,
-                body)
-      )
+      .setNotification(new Notification(title, body))
       .setToken(user.token)
       .setApnsConfig(appleConfig)
       .build();
 
-    // Send a message to the device corresponding to the provided
-    // registration token.
+    // Send a message to the device corresponding to the provided registration token.
     String response = null;
     try {
       response = FirebaseMessaging.getInstance().send(message);
@@ -59,7 +59,6 @@ public class UserNotifier {
   }
 
   public boolean cancellation(Artist artist, Journey waitingJourney, Journey cancelledJourney) {
-
     String title = artist.name + " has a cancellation and can see you!";
     String body = "Are you free on " + cancelledJourney.bookingDate;
 
@@ -99,7 +98,7 @@ public class UserNotifier {
   }
 
   public boolean filled(User successfulUser, Artist artist, Journey journey) {
-    
+
     String title = successfulUser.name + " got the slot!";
     String body = "The slot released by " + artist.name + " has been filled. " +
             "Don't worry there will be a next time.";
